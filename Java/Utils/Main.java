40cdacbf45ca398ee58496d8fd735667c67ce8b3
Bpham1 <brandon.pham@utexas.edu>
2020-04-25 11:49:47 -0500

    Added Sample Input and Output
    Updated main to test labelEnumerator

--- Java/Utils/Main.java
+++ Java/Utils/Main.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -19,5 +20,24 @@ public class Main {
             System.out.println();
         }
         csvreadwrter.write("SampleInput2.csv", csvreadwrter.getFeatureNames(), data);
+        List<String> testLabels = new ArrayList<String>();
+        testLabels.add("A");
+        testLabels.add("B");
+        testLabels.add("C");
+        for(String ele: testLabels){
+            System.out.print(ele + " ");
+        }
+        System.out.println();
+        LabelEnumerator le = new LabelEnumerator(testLabels);
+        List<Integer> enumeratedLabels = le.enumerateLabels(testLabels);
+        for(Integer ele: enumeratedLabels){
+            System.out.print(ele + " ");
+        }
+        System.out.println();
+        testLabels = le.labelEnumerates(enumeratedLabels);
+        for(String ele: testLabels){
+            System.out.print(ele + " ");
+        }
+        System.out.println();
     }
 }
