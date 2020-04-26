52bc25934f712e473e1537329db3b07488f09934
Bpham1 <brandon.pham@utexas.edu>
2020-04-26 18:34:22 -0500

    Added a sample input for RandomForest

--- Java/RandomForest/Main.java
+++ Java/RandomForest/Main.java
@@ -7,7 +7,7 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args){
         CSVReaderWriter csvreadwrter = new CSVReaderWriter();
-        System.out.println(csvreadwrter.read("C:\\Users\\brand\\IdeaProjects\\TermProject\\src\\SampleInput.csv"));
+        System.out.println(csvreadwrter.read("SampleInput.csv"));
         List<Integer> labels = csvreadwrter.getColumnAsInt("Y");
         List<List<Double>> data = csvreadwrter.getColumnRangeAsDouble("Feat 1", "Feat 3");
         for(Integer label: labels){
